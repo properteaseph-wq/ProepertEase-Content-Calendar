9ee6ce7581e8779d94b6bf64c7e6357d391a06b0
properteaseph-wq <properteaseph@gmail.com>
2026-02-22 22:10:57 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE = "propertease-pwa-v2";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./sw.js",
+  "./css/app.css",
+  "./js/app.js",
+  "./js/db.js",
+  "./js/ui.js",
+  "./js/prompt-engine.js",
+  "./assets/logo.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
